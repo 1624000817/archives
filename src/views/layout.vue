0bb867a2637c4d7dad758f0454046835e574969b
yuanxinxin <yuanxinxin@basechina.net>
2020-10-21 09:31:57 +0800

    首页

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -5,25 +5,25 @@
       <Aside></Aside>
       <el-main>
         <div class="conTop">
-            <div class="conTopL">{{$route.meta.name}}</div>
-            <div class="conTopR">
-                <div class="conTopRDiv">
-                登录ip：
-                <font class="conTopRDivC">100.0.0.0</font>
-                </div>
-                <div class="conTopRDiv">
-                登录地点：
-                <font class="conTopRDivC">无锡新区</font>
-                </div>
-                <div class="conTopRDiv">
-                登录时间：
-                <font class="conTopRDivC">2020-10-09 15:18:30 星期五</font>
-                </div>
-                <div class="conTopRDiv">
-                登录者：
-                <font class="conTopRDivC">张三</font>
-                </div>
+          <div class="conTopL">{{ $route.meta.name }}</div>
+          <div class="conTopR">
+            <div class="conTopRDiv">
+              登录ip：
+              <font class="conTopRDivC">100.0.0.0</font>
             </div>
+            <div class="conTopRDiv">
+              登录地点：
+              <font class="conTopRDivC">无锡新区</font>
+            </div>
+            <div class="conTopRDiv">
+              登录时间：
+              <font class="conTopRDivC">2020-10-09 15:18:30 星期五</font>
+            </div>
+            <div class="conTopRDiv">
+              登录者：
+              <font class="conTopRDivC">张三</font>
+            </div>
+          </div>
         </div>
         <router-view></router-view>
       </el-main>
@@ -38,6 +38,6 @@ export default {
   components: {
     Aside,
     Header,
-  }
+  },
 };
 </script>
